e03e17d67aea248fa7fa737fc7b888dd85027400
Chris Finn <chris@finn.com>
2021-09-06 18:48:19 -0400

    feat: pwa

---
+++ src/serviceWorkerRegistration.native.js
@@ -0,0 +1,7 @@
+// src/serviceWorkerRegistration.native.js
+
+// No-op on native. For offline support in native apps, check out expo-updates.
+
+export function register(config) {}
+
+export function unregister() {}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = async (env, argv) => {
   const config = await createExpoWebpackConfigAsync(env, argv);
+
+  if (env.mode === 'production') {
+    config.plugins.push(
+      // Generate a service worker script that will precache, and keep up to date,
+      // the HTML & assets that are part of the webpack build.
+      new WorkboxWebpackPlugin.InjectManifest({
+        swSrc: path.resolve(__dirname, 'src/service-worker.js'),
+        dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
+        exclude: [
+          /\.map$/,
+          /asset-manifest\.json$/,
+          /LICENSE/,
+          /\.js\.gz$/,
+          // Exclude all apple touch and chrome images because they're cached locally after the PWA is added.
+          /(apple-touch-startup-image|chrome-icon|apple-touch-icon).*\.png$/,
+        ],
+        // Bump up the default maximum size (2mb) that's precached,
+        // to make lazy-loading failure scenarios less likely.
+        // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      }),
+    );
+  }
 
   config.plugins = [
     ...config.plugins,
